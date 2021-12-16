6f1e933ec7db3751397ae98e3d0fdba845c50097
cristinaduran8 <93319981+cristinaduran8@users.noreply.github.com>
2021-12-16 11:45:33 +0100

    Add files via upload

---
+++ act2.py
@@ -0,0 +1,10 @@
+#cris
+#dona un nombre i doirà si es positiu,negatiu o cero 
+
+x = float(input("x= "))
+if x > 0:
+    print("x es positivo.")
+elif x < 0:
+    print("x no es positivo.")
+else: 
+    print("x es 0")
