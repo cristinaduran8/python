9f9f2f383ca3bdecbb0db044ef8cdc8035f2830b
cristinaduran8 <93319981+cristinaduran8@users.noreply.github.com>
2021-12-16 11:50:59 +0100

    Add files via upload

---
+++ act3.py
@@ -0,0 +1,8 @@
+#Escribe un programa que reciba un número del usuario e imprima ``Bravo!'' si es mayor a -10 y menor a 10, ambos incluidos.
+#cris
+
+x = float(input("x= "))
+if x > 10:
+    print("Bravo!")
+elif x < 10:
+    print("no:(")
