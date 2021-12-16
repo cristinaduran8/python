7b5ee328b07e29722a2268477fb9e0e6b5940c75
cristinaduran8 <93319981+cristinaduran8@users.noreply.github.com>
2021-12-16 12:02:04 +0100

    Add files via upload

---
+++ act7.py
@@ -0,0 +1,9 @@
+"""
+Hay cuatro errores en este código. Identifícalos. (4 pts)
+"""
+
+res = input("¿Cuál es el nombre del compañero de Sherlock Holmes?")
+if res == "Watson":
+    print("Correcto!")
+else:
+    print("Incorrecto! Es Watson.")
