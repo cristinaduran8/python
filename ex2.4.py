f7402ee416f25e3bce0010d8dd9e3cf5efdf2c1f
cristinaduran8 <93319981+cristinaduran8@users.noreply.github.com>
2021-12-17 09:27:45 +0100

    Add files via upload

---
+++ ex2.4.py
@@ -0,0 +1,9 @@
+#Cristina
+#Python
+
+"""
+Escribe un programa en Python que, usando un bucle for, imprima las palabras ``Rojo'' y ``Dorado'' 20 veces. 
+"""
+for i in range(20):
+    print("Rojo")
+    print("Dorado")
