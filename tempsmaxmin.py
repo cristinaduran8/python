d4eb12d664560bddfef8add42bfedae4f3cca1c4
cristinaduran8 <93319981+cristinaduran8@users.noreply.github.com>
2022-01-27 11:56:46 +0100

    Add files via upload

---
+++ tempsmaxmin.py
@@ -0,0 +1,29 @@
+"""
+CRISTINA DURÀN 
+EXERCICI:
+Una aplicació de seguretat controlaMaquina reb  una llistes de valors que indiquen les temperatures d’una màquina. 
+A cada rebuda de dades l’aplicació ha de imprimir el valor màxim i el mínim. També ha d’imprimir el nombre de vegades que ha superat el valor 3 (sols una vegada superat i desprès baixa i el torna superar, són dos pics).
+Exemples de valors:
+Entrada de valors: [1,2,4,4,5,6,7,1,2,5]
+Sortida de valors: “màxim: 7, mínim: 1, variat: 2
+Entrada de valors: [1,4,1,4,1,4]
+Sortida de valors: “màxim: 4, mínim: 1, variat: 3
+
+"""
+
+a = [5, 37, 4, 2, 10, 4, 6, 4, 7, 32, 9, 20]
+
+print("Aquest és el recomte de la temperatura")
+print(a)
+
+max = 0
+min = 1000
+
+for i in a:
+    if (i > max):
+        max = i
+    if (i < min):
+        min = i
+
+print('Valor màxim:', max)
+print('Valor mínim:', min)
